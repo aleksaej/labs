430c732fe1a67963467a1daabcbe3de0caffb4f3
aleksaej <132253473+aleksaej@users.noreply.github.com>
2024-09-30 18:28:39 +0300

    Add files via upload

---
+++ lab.1.3.cpp
@@ -0,0 +1,11 @@
+﻿int main() {
+	int nA = 95;
+	int ninverta = ~95;
+	int nB = 15, nC = 27;
+	int nOrbc = nB | nC;
+	int nAndbc = nB & nC;
+	int nXorBC = nB ^ nC;
+	int nLeftShift = nC << 3;
+	int nRightShift = nC >> 3;
+	return 0;
+}
